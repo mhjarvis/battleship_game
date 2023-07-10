f8cf4a1517cccea8d7cbff33c893e46c5170a498
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-07-10 11:49:48 -0500

    Add functions to create basic grid system in form

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import { Ship } from './ship';
 import { initializeGameButtons } from './gameButtons';
+import { createBoard } from './shipPlacement';
 
 (function () {
 
     initializeGameButtons();
+    createBoard();
+
 
 }())
---
+++ src/shipPlacement.js
@@ -0,0 +1,28 @@
+
+const getPlaceShipsBoard = document.querySelector('.place-ships-board');
+
+function createBoard() {
+    
+    for(let i = 0; i < 10; i++) {
+
+        const div = document.createElement('div');
+        div.setAttribute('id', 'box-container' + i);
+        div.setAttribute('class', 'box-container');
+        getPlaceShipsBoard.appendChild(div);
+
+        for(let j = 0; j < 10; j++) {
+
+            const boxContainer = document.querySelector('#box-container' + i);
+            const div = document.createElement('div');
+            div.setAttribute('class', 'box');
+            div.setAttribute('id', 'box' + i + j);
+            boxContainer.appendChild(div);
+        }
+    }
+    
+
+
+
+}
+
+export { createBoard }
