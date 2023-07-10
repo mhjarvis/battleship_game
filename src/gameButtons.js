f7c1956f7752579de5f05ecbba02b5ac0ec702b8
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-07-10 12:08:38 -0500

    Add funtionality to show/hide form element

--- src/gameButtons.js
+++ src/gameButtons.js
@@ -1,14 +1,22 @@
 function initializeGameButtons() {
 
+    const showForm = document.querySelector('.form-container');
+
     let startButton = document.querySelector('.start-game-btn')
     let resetButton = document.querySelector('.restart-game-btn')
+    let submitForm = document.querySelector('#submit-button')
 
     startButton.addEventListener('click', () => {
+        showForm.style.visibility = 'visible';
         console.log('start')
     })
 
     resetButton.addEventListener('click', () => {
         console.log('reset')
+    })
+
+    submitForm.addEventListener('click', () => {
+        showForm.style.visibility = 'hidden';
     })
 }
 
