fb195de77577799c709a1a1ff40529d38988f18a
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-07-11 08:55:21 -0500

    Add tests for the Ship factory function

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -5,4 +5,27 @@ describe('Ship factory function', () => {
     test('returns patrol boat length correctly', () => {
         expect(Ship(2).length).toBe(2)
     })
+    test('hits increase number of hits', () => {
+        let s1 = Ship(4);
+        s1.hit();
+        expect(s1.numberOfHits).toBe(1)
+    })
+    test('multiple hits return correct numberOfHits', () => {
+        let s1 = Ship(4);
+        s1.hit();
+        s1.hit();
+        s1.hit();
+        expect(s1.numberOfHits).toBe(3)
+    })
+    test('ship isSunk status remains false after ship takes less than its total hits', () => {
+        let s1 = Ship(3);
+        s1.hit();
+        expect(s1.sunk).toBe(false)
+    })
+    test('ship is sunk when receiving hits equal to its length', () => {
+        let s1 = Ship(2);
+        s1.hit();
+        s1.hit();
+        expect(s1.isSunk()).toBe(true);
+    })
 })
