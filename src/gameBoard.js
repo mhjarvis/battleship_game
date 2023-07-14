73a0dff989d3e804b72e1628df314be2026f3123
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-07-13 19:24:36 -0500

    Add loop to build all cordinate squares

---
+++ src/gameBoard.js
@@ -0,0 +1,24 @@
+// Create Gameboard
+const gamesBoardContainer = document.querySelector('#gamesboard-container')
+
+const width = 10
+
+function createBoard(color, user) {
+
+    const gameBoardContainer = document.createElement('div')
+    gameBoardContainer.classList.add('game-board')
+    gameBoardContainer.style.backgroundColor = color
+    gameBoardContainer.id = user
+
+    gamesBoardContainer.append(gameBoardContainer);
+
+    for (let i = 0; i < 100; i++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.id = i
+        gameBoardContainer.append(block)
+    }
+
+}
+
+export { createBoard }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import { initializeButtons } from "./gameButtons";
+import { createBoard } from "./gameBoard";
 
 (function () {
 
     initializeButtons();
+    createBoard('yellow', 'player');
+    createBoard('pink', 'computer');
+
+
 
 }())
+
 
 
