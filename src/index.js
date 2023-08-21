ac1cabf376c7074ca821a00aa400f899d56bbc34
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-08-21 15:35:16 -0500

    remove code and add created Gameboards

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import { Ship } from "./Ship";
+import { Gameboard } from "./Gameboard";
 
-let carrier = Ship(5)
-let battleship = Ship(4)
-let cruiser = Ship(3)
-let submarine = Ship(3)
-let destroyer = Ship(2)
+let playerBoard = Gameboard()
+let computerBoard = Gameboard()
+
+console.log(playerBoard)
 
 
-battleship.hit()
-battleship.hit()
-battleship.hit()
-console.log(battleship.isSunk())
 
-battleship.hit()
 
-console.log(battleship);
 
-console.log(battleship.isSunk())
+// npx webpack --watch
