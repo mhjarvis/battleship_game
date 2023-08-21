04e70910da9d5d8ea99c75c351f94d3e891135f5
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-08-20 21:35:30 -0500

    Add test for Ship object

--- src/tests/Ship.test.js
+++ src/tests/Ship.test.js
@@ -2,11 +2,14 @@ const funcs = require('../Ship');
 
 describe("Create Ship Object functions", () => {
 
-    test('hit function increases numberOfHits by 1', () => {
+    test('hit() function increases numberOfHits by 1', () => {
         let testShip = funcs.Ship(2)
-        testShip.hit();
-        
-        expect(testShip.hit().funcs..toBe(1))
+        testShip.hit()
+        expect(testShip.numberOfTimesHit).toEqual(1)
     })
-
+    test('isSunk() returns true when numberOfHits matches length', () => {
+        let testShip = funcs.Ship(1)
+        testShip.hit()
+        expect(testShip.isSunk()).toEqual(true)
+    })
 })
