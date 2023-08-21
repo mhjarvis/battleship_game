03dc609b80840ea545c98be31f55c4851347c584
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-08-20 21:37:25 -0500

    Update

--- src/Ship.js
+++ src/Ship.js
@@ -3,8 +3,9 @@ Begin your app by creating the Ship factory function.
     1. Your ‘ships’ will be objects that include their length, the number of times they’ve been hit, and whether or not they’ve been sunk.
     2. Ships should have a hit() function that increases the number of ‘hits’ in your ship.
     3. isSunk() should be a function that calculates it based on their length and the number of ‘hits’.
-***************************************************************/
 
+REMEMBER you only have to test your object’s public interface. Only methods or properties that are used outside of your ‘ship’ object need unit tests.
+***************************************************************/
 
 function Ship(length) {
     return {
