6923f43ab16607587116e9b265f1699ffcc12437
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-08-23 10:05:06 -0500

    Add collection of indivual board cells

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -1,5 +1,4 @@
 
-
 // hide cover
 
 function startInitialListeners() {
@@ -8,6 +7,18 @@ function startInitialListeners() {
     document.querySelector('.cover').addEventListener('click', function() {
         document.querySelector('.cover').style.display = 'none';
     })
+
+    // Add Even listeners to each individual board cell
+    const playerCells = document.querySelectorAll('.player-board-cell')
+    console.log(playerCells)
+    const computerCells = document.querySelectorAll('.computer-board-cell')
+    
+    for (let i = 0; i < playerCells.length; i++) {
+        playerCells[i].addEventListener('click', function() {
+            console.log('poop')
+        })
+    }
+
 }
 
 export { startInitialListeners }
