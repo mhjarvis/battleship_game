2ee85e1cf6774cddeb124ccf8ee99aa26455d36d
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-08-29 08:41:25 -0500

    Init

---
+++ src/Player.js
@@ -0,0 +1,11 @@
+/**************************************************************
+Create Player.
+    1. Players can take turns playing the game by attacking the enemy Gameboard.
+    2. The game is played against the computer, so make the ‘computer’ capable of making random plays. The AI does not have to be smart, but it should know whether or not a given move is legal. (i.e. it shouldn’t shoot the same coordinate twice).
+***************************************************************/
+
+class Player {
+    constructor() {
+        
+    }
+}
