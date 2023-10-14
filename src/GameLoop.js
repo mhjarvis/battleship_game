1d4efb45d313afbe41b57aa2457d4dff25a65eca
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-10-13 22:55:55 -0500

    Add id/class info to grid-cells

--- src/GameLoop.js
+++ src/GameLoop.js
@@ -7,8 +7,8 @@ function gameLoop() {
     const playerBoard = new Gameboard();
     const computerBoard = new Gameboard();
 
-    showBoardsInDOM('#player-board', playerBoard);
-    showBoardsInDOM('#computer-board', computerBoard);
+    showBoardsInDOM('#player-board', playerBoard, 'p');
+    showBoardsInDOM('#computer-board', computerBoard, 'c');
 
 
     console.log(`Creating computerBoard and playerBoard:`)
@@ -20,21 +20,16 @@ function gameLoop() {
 
 
 
-
-
-
-
-    // added div elements, without classes or ids or content;
-
-
-
 }
 
-function showBoardsInDOM(name, gBoard) {
+// Create board and append to DOM element
+function showBoardsInDOM(name, gBoard, prefix) {
     const board = document.querySelector(name);
 
     for (let i = 0; i < gBoard.board.length; i++) {
         const div = document.createElement('div');
+        div.class = grid-cell;
+        div.id = prefix + (i + 1);
         board.appendChild(div)
     }
 }
