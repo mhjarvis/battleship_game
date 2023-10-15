81805e5da88266ff96b6ffa059ceec2773239f40
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-10-15 01:16:00 -0500

    Update; ondrop currently not working

--- src/addListeners.js
+++ src/addListeners.js
@@ -8,10 +8,31 @@ export function addListeners() {
     const destroyer = document.getElementById('destroyer');
 
     let arr = [carrier, battleship, cruiser, submarine, destroyer];
+    let gridCell = document.querySelectorAll('.grid-cell');
     
     for (let a of arr) {
         a.addEventListener('dragstart', (event) => {
-            console.log(event.target.id);
+            console.log('sdfsf');
+        })
+        a.addEventListener('dragend', (event) => {
+            event.preventDefault();
+        })
+    }
+
+    for (let g of gridCell) {
+        g.addEventListener('dragenter', (event) => {
+            console.log('poo')
+            event.preventDefault();
+        })
+        g.addEventListener('ondragover', (event) => {
+            event.preventDefault();
+        })
+        g.addEventListener('ondragleave', (event) => {
+            event.preventDefault();
+        })
+        g.addEventListener('ondrop', (event) => {
+            //event.preventDefault();
+            console.log('wefwfewewfwefewff')
         })
     }
 
