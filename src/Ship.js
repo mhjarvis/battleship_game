750f30a026a4822554c75ae32bd59b9534d24646
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-12-18 09:56:44 -0600

    Set up Ship.js class

--- src/Ship.js
+++ src/Ship.js
@@ -1,1 +1,16 @@
-
+class Ship {
+    constructor(length, name) {
+        this.length = length,
+        this.name = name,
+        this.numberOfHits = 0,
+        this.isSunk = false
+    }
+    hit() {
+        this.numberOfHits += 1;
+    }
+    isSunk() {
+        if (this.length === this.numberOfHits) {
+            this.isSunk = true;
+        }
+    }
+}
