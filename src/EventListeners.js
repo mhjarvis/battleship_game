31e608f2119d780bdcdfa9380424f6cd0bea5e9e
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-12-18 21:38:40 -0600

    refactor

--- src/EventListeners.js
+++ src/EventListeners.js
@@ -1,43 +1,41 @@
 function deployListeners() {
-
     const flip = document.querySelector('#flip');
     const ships = document.querySelectorAll('.ship-piece');
-    const shipContainer = document.querySelector('.ship-container');
-
 
     // FLIP Button changes height/width of each element
     // There may be an easier method for doing this
     flip.addEventListener('click', () => {
         for (let ship of ships) {
-            let id = document.querySelector(`#${ship.id}`);
-
-            if (id.classList.contains('test')) {
-                id.classList.remove('test');
-
-                let w = id.clientWidth;
-                let h = id.clientHeight;
-                id.style.width = `${h}px`;
-                id.style.height = `${w}px`;
-
-                shipContainer.style.flexDirection = 'row';
-
-            } else {
-                id.classList.add('test');
-
-                let w = id.clientWidth;
-                let h = id.clientHeight;
-                id.style.width = `${h}px`;
-                id.style.height = `${w}px`;
-
-                shipContainer.style.flexDirection = 'column';
-            }
+            changeHW(ship);
         }
     })
+}
 
-    // 
+const changeHW = (ship) => {
+    let id = document.querySelector(`#${ship.id}`);
+    const shipContainer = document.querySelector('.ship-container');
 
 
+    if (id.classList.contains('test')) {
+        id.classList.remove('test');
 
+        let w = id.clientWidth;
+        let h = id.clientHeight;
+        id.style.width = `${h}px`;
+        id.style.height = `${w}px`;
+
+        shipContainer.style.flexDirection = 'row';
+
+    } else {
+        id.classList.add('test');
+
+        let w = id.clientWidth;
+        let h = id.clientHeight;
+        id.style.width = `${h}px`;
+        id.style.height = `${w}px`;
+
+        shipContainer.style.flexDirection = 'column';
+    }
 }
 
 export { deployListeners }
