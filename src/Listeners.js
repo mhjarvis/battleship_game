de6c8b5e4d517a923dfba34d8e402246f84dd5b8
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-01-02 15:41:29 -0600

    Add eventListeners to toggle 'hidden' class

--- src/Listeners.js
+++ src/Listeners.js
@@ -2,12 +2,25 @@ import { placePlayerShips } from "./Gameloop";
 
 function initiateListeners() {
 
-    let randomizeButton = document.getElementById('randomize');
+    const randomizeButton = document.getElementById('randomize-button')
+    const startButton = document.getElementById('start-button')
+    const resetButton = document.getElementById('reset-button')
 
     randomizeButton.addEventListener('click', () => {
         placePlayerShips()
     })
 
+    startButton.addEventListener('click', () => {
+        randomizeButton.classList.toggle('hidden')
+        startButton.classList.toggle('hidden')
+        resetButton.classList.toggle('hidden')
+    })
+
+    resetButton.addEventListener('click', () => {
+        randomizeButton.classList.toggle('hidden')
+        startButton.classList.toggle('hidden')
+        resetButton.classList.toggle('hidden')
+    })
 
 
 
